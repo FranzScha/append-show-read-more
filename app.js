53090aa14add30e044a1f96eb5105dfbd86b9491
FranzScha <94545492+FranzScha@users.noreply.github.com>
2022-11-16 22:35:03 +0800

    Corrected error
    v1.1

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const toggle = document.querySelectorAll(".toggleMore");
+const iconSwitch = document.querySelectorAll(".switch");
 const displayContent = document.querySelectorAll(".show-more-content");
+
 const fadeEffect = document.querySelector(".target");
 const fadeEffect2 = document.querySelector(".targe-1");
 const fadeEffect3 = document.querySelector(".targe-2");
 
+let isIconActive = false;
+
 toggle.forEach((button, index) => {
   button.addEventListener("click", () => {
     displayContent.forEach((element, idx) => {
+      // show the hidden content
       let getAttribute = element.getAttribute("data-show");
       if (idx === index && getAttribute === "false") {
         element.removeAttribute("data-close");
@@ -20,10 +25,41 @@ toggle.forEach((button, index) => {
           },
           { once: true }
         );
+        isIconActive = true;
+      }
+      // click another service the close the previous one
+      if (getAttribute === "true") {
+        element.setAttribute("data-show", "false");
+        element.addEventListener(
+          "animationend",
+          () => {
+            element.setAttribute("data-close", "");
+          },
+          { once: true }
+        );
       }
     });
+    iconChange(index);
   });
 });
+
+// switch the icon
+function iconChange(index) {
+  iconSwitch.forEach((el, idx) => {
+    if (idx === index) {
+      el.classList.remove("fa-plus");
+      el.classList.add("fa-minus");
+    } else if (idx !== index) {
+      el.classList.remove("fa-minus");
+      el.classList.add("fa-plus");
+    }
+    if (idx === index && isIconActive === true) {
+      el.classList.add("fa-plus");
+      isIconActive = false;
+      console.log(isIconActive, "3");
+    }
+  });
+}
 
 // fade effect
 fadeEffect.addEventListener("mouseover", () => {
